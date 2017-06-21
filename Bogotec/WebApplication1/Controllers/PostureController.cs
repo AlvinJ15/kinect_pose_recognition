a39450289ea2ae245ba69b8a4ae41a5f9c74fad9
alvinjr15 <alvin@episunsa.edu.pe>
2017-06-21 00:21:37 -0500

    - Add REST Api

---
+++ Bogotec/WebApplication1/Controllers/PostureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+//using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/posture")]
+    public class PostureController : ApiController
+    {
+        // GET api/posture/5
+        [Route("validate/{id}")]
+        public string Get(string id)
+        {
+            return "posture";
+        }
+    }
+}
